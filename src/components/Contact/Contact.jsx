3f3cf133295bfe36c0ce0436e32d113ae07e2df7
Justin Brierley <91489058+paradingwolves@users.noreply.github.com>
2025-04-29 15:48:15 -0400

    Update Contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,23 +1,30 @@
 import React from 'react';
 import ContactForm from './ContactForm';
+import Header from '../Layout/Header/Header';
+import Footer from '../Layout/Footer/Footer';
 
 const Contact = () => {
   return (
-    <div className="container d-flex flex-column justify-content-center align-items-center text-center mt-1">
-      <h2 className="mb-3">Contact Support</h2>
-      <p className="mb-4">If you need help or have any questions, please reach out to us:</p>
-      <ul className="list-unstyled">
-        <li className="mb-2">
-          <strong>📞 Phone:</strong>{' '}
-          <a href="tel:5199961094" className="text-decoration-none"> (519) 996-1094</a>
-        </li>
-        <li>
-          <strong>📧 Email:</strong>{' '}
-          <a href="mailto:justinbrierley3@outlook.com" className="text-decoration-none">justinbrierley3@outlook.com</a>
-        </li>
-      </ul>
-      <ContactForm />
-    </div>
+    <>
+      <Header />
+      <div className="container d-flex flex-column justify-content-center align-items-center text-center mt-1">
+        <h2 className="mb-3">Contact Support</h2>
+        <p className="mb-4">If you need help or have any questions, please reach out to us:</p>
+        <ul className="list-unstyled">
+          <li className="mb-2">
+            <strong>📞 Phone:</strong>{' '}
+            <a href="tel:5199961094" className="text-decoration-none"> (519) 996-1094</a>
+          </li>
+          <li>
+            <strong>📧 Email:</strong>{' '}
+            <a href="mailto:justinbrierley3@outlook.com" className="text-decoration-none">justinbrierley3@outlook.com</a>
+          </li>
+        </ul>
+        <ContactForm />
+      </div>
+      <Footer />
+    </>
+    
   );
 };
 
